72a6c28447b4c347f30698f3a697d14f9cee4fe1
acsandmann <157552025+acsandmann@users.noreply.github.com>
2026-09-09 13:05:57 -0400

    yes

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -26,11 +26,6 @@ export default defineConfig({
       ],
       social: [
         {
-          icon: "blueSky",
-          label: "BlueSky",
-          href: "https://bsky.app/profile/trueberryless.org",
-        },
-        {
           icon: "github",
           label: "GitHub",
           href: "https://github.com/acsandmann/starlight-theme-apple",
